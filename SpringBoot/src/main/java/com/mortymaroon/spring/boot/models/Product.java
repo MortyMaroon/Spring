a2487f6bc0d8946a470b77901199be5775baff8b
MortyMaroon <51970904+MortyMaroon@users.noreply.github.com>
2020-12-14 20:17:10 +0300

    Домашнее работа к 4 уроку

---
+++ SpringBoot/src/main/java/com/mortymaroon/spring/boot/models/Product.java
@@ -0,0 +1,14 @@
+package com.mortymaroon.spring.boot.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String title;
+    private int price;
+}
