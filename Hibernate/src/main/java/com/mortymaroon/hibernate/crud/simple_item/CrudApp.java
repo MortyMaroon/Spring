b06920f4ecd4babafbc8e3e7d34a01c919011d74
MortyMaroon <51970904+MortyMaroon@users.noreply.github.com>
2020-12-19 14:22:11 +0300

    Пример программы с использованием Hibernate

--- Hibernate/src/main/java/com/mortymaroon/hibernate/crud/simple_item/CrudApp.java
+++ Hibernate/src/main/java/com/mortymaroon/hibernate/crud/simple_item/CrudApp.java
@@ -5,6 +5,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class CrudApp {
     private static SessionFactory factory;
 
@@ -19,15 +21,56 @@ public class CrudApp {
         factory.close();
     }
 
+    public static void showManyItems() {
+        try (Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+
+            List<SimpleItem> items = session.createQuery("from SimpleItem").getResultList();
+            System.out.println(items + "\n");
+
+            SimpleItem si1 = session.createQuery("select s from SimpleItem s where s.id = 3", SimpleItem.class).getSingleResult();
+            System.out.println(si1 + "\n");
+
+            List<SimpleItem> cheapItems = session.createQuery("select s from SimpleItem s where s.price < 80").getResultList();
+            System.out.println(cheapItems + "\n");
+
+            session.getTransaction().commit();
+        }
+    }
+
     public static void createExample() {
         try(Session session = factory.getCurrentSession()) {
             session.beginTransaction();
             SimpleItem dragonStatue = new SimpleItem("Dragon Statue",100);
-            System.out.println(dragonStatue);
             session.saveOrUpdate(dragonStatue);
-            System.out.println(dragonStatue);
             session.getTransaction().commit();
-            System.out.println(dragonStatue);
+        }
+    }
+
+    public static void readAndPrintExample() {
+        try (Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+            SimpleItem simpleItem = session.get(SimpleItem.class, 1L);
+            System.out.println(simpleItem);
+            session.getTransaction().commit();
+        }
+    }
+
+    public static void updateExample() {
+        try(Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+            SimpleItem simpleItem = session.get(SimpleItem.class, 1L);
+            session.delete(simpleItem);
+            session.getTransaction().commit();
+        }
+    }
+
+    public static void deleteExample() {
+        try(Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+            SimpleItem simpleItem = session.get(SimpleItem.class, 1L);
+            simpleItem.setPrice(10000);
+            session.getTransaction().commit();
         }
     }
 
@@ -35,6 +78,9 @@ public class CrudApp {
         try {
             init();
             createExample();
+            readAndPrintExample();
+            updateExample();
+            deleteExample();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
