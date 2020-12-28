7bfa86c658a4de9ae8adec09ed2c57abefe8de38
MortyMaroon <51970904+MortyMaroon@users.noreply.github.com>
2020-12-28 23:37:17 +0300

    Пример использования Spring Data

---
+++ SpringData/src/main/java/com/mortymaroon/springdata/mortymaroon/MortymaroonApplication.java
@@ -0,0 +1,13 @@
+package com.mortymaroon.springdata.mortymaroon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortymaroonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortymaroonApplication.class, args);
+	}
+
+}
---
+++ SpringData/src/test/java/com/mortymaroon/springdata/mortymaroon/MortymaroonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mortymaroon.springdata.mortymaroon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortymaroonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
